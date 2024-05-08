c1e5c7452c341de61ef91e5e5864097e2a97194b
GianmarcoBordin <bordingianmarco@virgilio.it>
2024-05-08 12:00:26 +0200

    fix

--- android/src/com/mygdx/game/presentation/MainViewModel.kt
+++ android/src/com/mygdx/game/presentation/MainViewModel.kt
@@ -40,21 +40,16 @@ class MainViewModel @Inject constructor(
     var startDestination by mutableStateOf(Route.AppStartNavigation.route)
     init {
         appEntryUseCases.readAppEntry().onEach { shouldStartFromHomeScreen ->
-            entered = true
             startDestination = if (shouldStartFromHomeScreen) {
                 if (authenticationUseCases.authCheck()) {
                     Route.HomeScreen.route
                 } else {
                     Route.SignInScreen.route
                 }
-            } else {
+            }else {
                 Route.OnBoardingScreen.route
             }
         }
-        if (!entered) {
-            startDestination = Route.OnBoardingScreen.route
-        }
-        Log.d ("MAIN VIEW", "Start destination $startDestination")
     }
 
     override fun onCleared() {
