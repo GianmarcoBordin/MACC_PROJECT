e4bc3df09e5d8a7fa1283660aa17915b8cd2e790
Carlo <giralda.carlo@gmail.com>
2024-05-12 11:06:11 +0200

    Fix

--- android/src/com/mygdx/game/domain/usecase/ar/ARUseCases.kt
+++ android/src/com/mygdx/game/domain/usecase/ar/ARUseCases.kt
@@ -3,7 +3,6 @@ package com.mygdx.game.domain.usecase.ar
 data class ARUseCases (
     val addGameItem: AddGameItem,
     val getGameItem: GetGameItem,
-    val getGameItemsUser: GetGameItemsUser,
     val addOwnership: AddOwnership,
     val getOwnership: GetOwnership
 )
